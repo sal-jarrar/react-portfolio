177ee8edd31a19763afb5952aa420071314f312a
sal-jarrar <eng.sultanj@gmail.com>
2023-05-01 11:27:46 -0700

    Update projects

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,35 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
+import Expen from "../assets/projects/Expen.png";
+import Covid from "../assets/projects/covid.png";
+import GPT3 from "../assets/projects/gpt3.png";
+import Admin from "../assets/projects/admin.png";
 
-export const data=[
-    {
-        id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:2,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
-    {
-        id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
-
-
-]
+export const data = [
+  {
+    id: 1,
+    name: "GPT-3",
+    image: GPT3,
+    github: "https://github.com/sal-jarrar/gpt3_sal",
+    live: "https://gpt-3-dnr2.onrender.com/",
+  },
+  {
+    id: 2,
+    name: "Admin Dashboard",
+    image: Admin,
+    github: "",
+    live: "https://dashboard-client-mzn9.onrender.com",
+  },
+  {
+    id: 3,
+    name: "Covid-Tracker",
+    image: Covid,
+    github: "https://github.com/sal-jarrar/VoiceExpenseTracker-React",
+    live: "https://deluxe-daffodil-c68fd4.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Speechly Expense",
+    image: Expen,
+    github: "https://github.com/sal-jarrar/VoiceExpenseTracker-React",
+    live: "https://clinquant-vacherin-14e214.netlify.app/",
+  },
+];
